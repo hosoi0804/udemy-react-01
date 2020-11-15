09543603896663e16fc093ad32b1213571711d8a
hosoi0804 <mb.takahiro.hosoi@gmail.com>
2020-11-15 13:13:40 +0900

    func mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 // eslint-disable-next-line
-import React, { Component } from 'react';
+import React from 'react';
 
-function App() {
-  return <div><h1>Hello World!</h1></div>;
+// function App() {
+//   // const greeting = "Hi, Tom!";
+//   // const dom = <h1 className="foo">{greeting}</h1>;
+//   // return dom;
+
+//   return (
+//     <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={() => {console.log("I am clicked!")}}/>
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Neow!</div>
 }
 
 export default App;
